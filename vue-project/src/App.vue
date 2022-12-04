0cb772421fba85a342d75ca08f3c0881c52a0316
Persenlo <1140086781@qq.com>
2022-12-04 15:34:05 +0800

    结构更新

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,85 +1,42 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <a-layout>
+    <a-layout-header>
+      <HeaderVue/>
+    </a-layout-header>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+    <a-layout>
 
-  <RouterView />
+      <a-layout-sider>
+        <SiderVue/>
+      </a-layout-sider>
+
+      <a-layout-content>
+        <MainVue/>
+      </a-layout-content>
+
+    </a-layout>
+
+    <a-layout-footer>
+      <FooterVue/>
+    </a-layout-footer>
+
+  </a-layout>
+
+
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+  
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+<script setup>
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
+import HeaderVue from './layout/Header.vue';
+import SiderVue from './layout/Sider.vue'
+import MainVue from './layout/Main.vue';
+import FooterVue from './layout/Footer.vue';
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
+</script>
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
+<style>
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -4,9 +4,14 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+import { DatePicker } from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+
+import './assets/css/base.css'
 
 const app = createApp(App)
+
+app.use(DatePicker)
 
 app.use(createPinia())
 app.use(router)
